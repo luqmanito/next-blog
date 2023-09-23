0ff1bcbaef1e88dcdc965a5a10844f82de8264f3
luqmanito <luqman@millennia-solusi.id>
2023-09-23 14:10:42 +0700

    first

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,25 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Link href="/">
+        <h1>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum,
+          aperiam?
+        </h1>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores
+        blanditiis unde, nam delectus eos dignissimos, ipsam molestias eveniet
+        sint, aliquid sequi earum architecto ut suscipit perferendis maxime
+        quisquam odit! Reiciendis? Lorem ipsum dolor, sit amet consectetur
+        adipisicing elit. Dolores blanditiis unde, nam delectus eos dignissimos,
+        ipsam molestias eveniet sint, aliquid sequi earum architecto ut suscipit
+        perferendis maxime quisquam odit! Reiciendis? Lorem ipsum dolor, sit
+        amet consectetur adipisicing elit. Dolores blanditiis unde, nam delectus
+        eos dignissimos, ipsam molestias eveniet sint, aliquid sequi earum
+        architecto ut suscipit perferendis maxime quisquam odit! Reiciendis?
+      </Link>
+    </div>
+  );
 }
